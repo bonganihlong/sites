016bee66a36875eb296b31d3fc429cdc51f369aa
Kukhany Hlongwane <kukhanyahlongwane@live.com>
2022-09-14 07:44:01 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 if (navigator && navigator.serviceWorker) {
     var serviceWorker = navigator.serviceWorker.register('./sw.js');
-    navigator.serviceWorker.addEventListener('message', (message) => {console.log(message);})
+    //navigator.serviceWorker.addEventListener('message', (message) => {console.log(message);})
 }
 
 
 if (navigator && navigator.storage) {
     let storageManager = navigator.storage
-    storageManager.estimate().then((result)=> console.log(result));
+    //storageManager.estimate().then((result)=> console.log(result));
 }
 
 
